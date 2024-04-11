0253b65553628e4bea7d1e1a102e8c3f076b4547
Ignatius <39426896+YanbinHe@users.noreply.github.com>
2024-04-11 15:36:41 +0200

    Delete deco_KroSBL_noisy directory

--- deco_KroSBL_noisy/functions/prox_l1.m
+++
@@ -1,7 +0,0 @@
-function N = prox_l1(u, alpha)
-    N = length(u);
-    re = zeros(N,1);
-    upos = find(abs(u)>=alpha); 
-    output = exp(1i * angle(u)).*(abs(u) - alpha);
-    re(upos) = output(upos);
-end
--- deco_KroSBL_noisy/parsave.m
+++
@@ -1,4 +0,0 @@
-function parsave(avg,x,y)
-fname = ['./results/noiseless_compare_', num2str(avg),'.mat'];
-save(fname,'x','y')
-end
